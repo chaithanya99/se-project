9699ded5c52663d67c740de7d90ea5c0514f5be1
Chaithanya Reddy <79004348+chaithanya99@users.noreply.github.com>
2022-11-25 10:21:49 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -6,12 +6,8 @@ from add import addition
 
 class TestSum(unittest.TestCase):
     def test_list_int(self):
-        """
-        Test case to add two numbers
-        """
-        #data = [20, 30]
         result = addition(2,3)
-        self.assertEqual(result, 500)
+        self.assertEqual(result, 5)
 
 if __name__ == '__main__':
     unittest.main()
